319e1ae6f0be772cb26c8d2849dc7dfd8c3e9422
razvantom <97461434+razvantom@users.noreply.github.com>
2026-02-24 14:23:34 +0000

    feat: backend nestjs + prisma init

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,8 @@
-console.log("ARCA Backend bootstrap OK");
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
